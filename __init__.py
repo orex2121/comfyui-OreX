ecd837cd0233d161b2d95851f821695174334cd0
OreX <65088103+orex2121@users.noreply.github.com>
2026-05-19 23:19:09 +0300

    19052026

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .OreXImageLoadBatch import OreXImageLoadBatch
 from .OreXImageLoadBatchSize import OreXImageLoadBatchSize
 from .OreXKontextPresets import KontextPresetsOrex
 from .OreX_LMStudio import OreXLMStudio
+from .OreX_Ollama import OreXOllama
 
 NODE_CLASS_MAPPINGS = {
     "orex Load Image": OreXImageLoad,
@@ -11,7 +12,8 @@ NODE_CLASS_MAPPINGS = {
     "orex Load Image Batch": OreXImageLoadBatch,
     "orex Load Image Batch Size": OreXImageLoadBatchSize,
     "orex Kontext Presets": KontextPresetsOrex,
-    "orex LMStudio": OreXLMStudio
+    "orex LMStudio": OreXLMStudio,
+    "orex Ollama": OreXOllama
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -20,7 +22,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "orex Load Image Batch": "📦 Load Image Batch (OreX)",
     "orex Load Image Batch Size": "📦 Load Image Batch Size (OreX)",
     "orex Kontext Presets": "📦 Kontext Presets (OreX)",
-    "orex LMStudio": "🤖 LMStudio (OreX)"
+    "orex LMStudio": "🤖 LMStudio (OreX)",
+    "orex Ollama": "🦙 Ollama (OreX)"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
